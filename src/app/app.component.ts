17449d3f2dc704023f2e5789778728997f3ad6f1
eprequejo <erequejo@companywatch.net>
2022-01-26 11:01:52 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Database, getDatabase, ref, set } from "firebase/database";
+import { Auth, getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-firebase-app';
+  user = { name: "", age: "" };
+
+  // app: FirebaseApp;
+  database: Database;
+  auth: Auth;
+
+  constructor() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBCmMuTHrNJLGXgoR2wy3_kBkUvB2J7n-8",
+        authDomain: "angular-firebase-app-affce.firebaseapp.com",
+        databaseURL: "https://angular-firebase-app-affce-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "angular-firebase-app-affce",
+        storageBucket: "angular-firebase-app-affce.appspot.com",
+        messagingSenderId: "185900307509",
+        appId: "1:185900307509:web:6d7e9b29981bd487f48d7c",
+        measurementId: "G-9Z1TFY8DYR"
+      };
+    const app = initializeApp(firebaseConfig);
+    this.database = getDatabase(app);
+    this.auth = getAuth(app);
+  }
+
+  submit() {
+    signInAnonymously(this.auth).then(() => {
+      onAuthStateChanged(this.auth, (u) => {
+        if (u) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          const uid = u.uid;
+          set(ref(this.database, "users/" + uid), {
+            name: this.user.name,
+            age: this.user.age,
+            created: new Date().toISOString().split("T")[0]
+          });
+        } else {
+          throw new Error("User is not signed in")
+        }
+      });
+    }).catch((error) => {
+      throw new Error(`${error.code}: ${error.message}`)
+    });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
